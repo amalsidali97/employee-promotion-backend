69a1c3a95b15eddcd9b8d567225547f4c7e74515
amalsidali97 <84085641+amalsidali97@users.noreply.github.com>
2021-05-27 17:17:38 +0100

    update

--- employee_promotion_backend/config.py
+++ employee_promotion_backend/config.py
@@ -2,12 +2,12 @@
 
 MYSQL_HOST = '127.0.0.1' 
 MYSQL_USERNAME = 'root'
-MYSQL_PASSWORD = 'greenoi95'
-MYSQL_DATABASE = 'python_mysql'
+MYSQL_PASSWORD = 'Esi2021'
+MYSQL_DATABASE = 'rhdb'
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+MYSQL_USERNAME+':'+MYSQL_PASSWORD+'@'+MYSQL_HOST+'/'+MYSQL_DATABASE
 SERVER_HOST = '0.0.0.0'
 KEY_JWT = 'mySecret'  # Prod
 MAIL_SERVER = 'smtp.gmail.com'
-MAIL_USERNAME = 'redaabdou49@gmail.com'
-MAIL_PASSWORD = ''
+MAIL_USERNAME = 'amalsidali.mk@gmail.com'
+MAIL_PASSWORD = 'tnnbpivhfrnstcjl'
